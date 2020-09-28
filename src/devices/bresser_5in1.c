5be10b561450b8de8279d3e624c478729fb979da
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2020-09-28 12:58:39 +0200

    Add support for Bresser 6-in-1 weather station

--- src/devices/bresser_5in1.c
+++ src/devices/bresser_5in1.c
@@ -11,6 +11,122 @@
 */
 
 /**
+Decoder for Bresser Weather Center 6-in-1.
+
+{206}55555555545ba83e803100058631ff11fe6611ffffffff01cc00 [Hum 96% Temp 3.8 C Wind 0.7 m/s]
+{205}55555555545ba999263100058631fffffe66d006092bffe0cff8 [Hum 95% Temp 3.0 C Wind 0.0 m/s]
+{199}55555555545ba840523100058631ff77fe668000495fff0bbe [Hum 95% Temp 3.0 C Wind 0.4 m/s]
+{205}55555555545ba94d063100058631fffffe665006092bffe14ff8
+{206}55555555545ba860703100058631fffffe6651ffffffff0135fc [Hum 95% Temp 3.0 C Wind 0.0 m/s]
+{205}55555555545ba924d23100058631ff99fe68b004e92dffe073f8 [Hum 96% Temp 2.7 C Wind 0.4 m/s]
+{202}55555555545ba813403100058631ff77fe6810050929ffe1180 [Hum 94% Temp 2.8 C Wind 0.4 m/s]
+{205}55555555545ba98be83100058631fffffe6130050929ffe17800 [Hum 95% Temp 2.8 C Wind 0.8 m/s]
+
+                                          TEMP  HUM
+2dd4  1f 40 18 80 02 c3 18 ff 88 ff 33 08 ff ff ff ff 80 e6 00 [Hum 96% Temp 3.8 C Wind 0.7 m/s]
+2dd4  cc 93 18 80 02 c3 18 ff ff ff 33 68 03 04 95 ff f0 67 3f [Hum 95% Temp 3.0 C Wind 0.0 m/s]
+2dd4  20 29 18 80 02 c3 18 ff bb ff 33 40 00 24 af ff 85 df    [Hum 95% Temp 3.0 C Wind 0.4 m/s]
+2dd4  a6 83 18 80 02 c3 18 ff ff ff 33 28 03 04 95 ff f0 a7 3f
+2dd4  30 38 18 80 02 c3 18 ff ff ff 33 28 ff ff ff ff 80 9a 7f [Hum 95% Temp 3.0 C Wind 0.0 m/s]
+2dd4  92 69 18 80 02 c3 18 ff cc ff 34 58 02 74 96 ff f0 39 3f [Hum 96% Temp 2.7 C Wind 0.4 m/s]
+2dd4  09 a0 18 80 02 c3 18 ff bb ff 34 08 02 84 94 ff f0 8c 0  [Hum 94% Temp 2.8 C Wind 0.4 m/s]
+2dd4  c5 f4 18 80 02 c3 18 ff ff ff 30 98 02 84 94 ff f0 bc 00 [Hum 95% Temp 2.8 C Wind 0.8 m/s]
+
+
+{147} 5e aa 18 80 02 c3 18 fa 8f fb 27 68 11 84 81 ff f0 72 00  Temp 11.8 C  Hum 81%
+{149} ae d1 18 80 02 c3 18 fa 8d fb 26 78 ff ff ff fe 02 db f0
+{150} f8 2e 18 80 02 c3 18 fc c6 fd 26 38 11 84 81 ff f0 68 00  Temp 11.8 C  Hum 81%
+{149} c4 7d 18 80 02 c3 18 fc 78 fd 29 28 ff ff ff fe 03 97 f0
+{149} 28 1e 18 80 02 c3 18 fb b7 fc 26 58 ff ff ff fe 02 c3 f0
+{150} 21 e8 18 80 02 c3 18 fb 9c fc 33 08 11 84 81 ff f0 b7 f8  Temp 11.8 C  Hum 81%
+{149} 83 ae 18 80 02 c3 18 fc 78 fc 29 28 ff ff ff fe 03 98 00
+{150} 5c e4 18 80 02 c3 18 fb ba fc 26 98 11 84 81 ff f0 16 00  Temp 11.8 C  Hum 81%
+{148} d0 bd 18 80 02 c3 18 f9 ad fa 26 48 ff ff ff fe 02 ff f0
+
+    CHK?8h8h ID?8h8h8h8h8h 8h8h8h WDIR:12h 4h TEMP8h.4h ?4h HUM8h UV?~12h 4h 8h8h
+*/
+
+#include "decoder.h"
+
+static int bresser_6in1_callback(r_device *decoder, bitbuffer_t *bitbuffer)
+{
+    uint8_t const preamble_pattern[] = {0xaa, 0xaa, 0x2d, 0xd4};
+
+    data_t *data;
+    uint8_t msg[20];
+    uint16_t sensor_id;
+    unsigned len = 0;
+
+    if (bitbuffer->num_rows != 1
+            || bitbuffer->bits_per_row[0] < 160
+            || bitbuffer->bits_per_row[0] > 220) {
+        if (decoder->verbose > 1) {
+            fprintf(stderr, "%s bit_per_row %u out of range\n", __func__, bitbuffer->bits_per_row[0]);
+        }
+        return DECODE_ABORT_EARLY; // Unrecognized data
+    }
+
+    unsigned start_pos = bitbuffer_search(bitbuffer, 0, 0,
+            preamble_pattern, sizeof (preamble_pattern) * 8);
+
+    if (start_pos >= bitbuffer->bits_per_row[0]) {
+        return DECODE_ABORT_LENGTH;
+    }
+    start_pos += sizeof (preamble_pattern) * 8;
+    len = bitbuffer->bits_per_row[0] - start_pos;
+
+    if (len < 144) {
+        if (decoder->verbose > 1) {
+            fprintf(stderr, "%s: %u too short\n", __func__, len);
+        }
+        return DECODE_ABORT_LENGTH; // message too short
+    }
+    // truncate any excessive bits
+    len = MIN(len, sizeof (msg) * 8);
+
+    bitbuffer_extract_bytes(bitbuffer, 0, start_pos, msg, len);
+
+    bitrow_printf(msg, len, "%s: ", __func__);
+
+    int wind_dir = ((msg[10] & 0xf0) >> 4) * 100 + (msg[10] & 0x0f) * 10 + ((msg[11] & 0xf0) >> 4);
+
+    int temp_ok = msg[12] != 0xff;
+    int temp_raw = ((msg[12] & 0xf0) >> 4) * 100 + (msg[12] & 0x0f) * 10 + ((msg[13] & 0xf0) >> 4);
+    //if (msg[25] & 0x0f)
+    //    temp_raw = -temp_raw;
+    float temperature = temp_raw * 0.1f;
+
+    int humidity_ok = msg[14] != 0xff;
+    int humidity = (msg[14] & 0x0f) + ((msg[14] & 0xf0) >> 4) * 10;
+
+    //int uv_ok = (msg[16] & 0x0f) == 0;
+    int uv_ok  = (msg[16] & 0xf0) != 0xf0;
+    int uv_raw = ((msg[15] & 0xf0) >> 4) * 100 + (msg[15] & 0x0f) * 10 + ((msg[16] & 0xf0) >> 4);
+    float uv   = uv_raw * 0.1f;
+
+    int rain_ok  = (msg[16] & 0xf0) == 0xf0;
+    int rain_raw = ((msg[15] & 0xf0) >> 4) * 10 + (msg[15] & 0x0f);
+
+    //    CHK?8h8h ID?8h8h8h8h8h 8h8h8h WDIR:12h 4h TEMP8h.4h ?4h HUM8h UV?~12h 4h 8h8h
+
+    /* clang-format off */
+    data = data_make(
+            "model",            "",             DATA_STRING, "Bresser-6in1",
+            //"id",               "",             DATA_INT,    sensor_id,
+            "temperature_C",    "Temperature",  DATA_COND, temp_ok, DATA_FORMAT, "%.1f C", DATA_DOUBLE, temperature,
+            "humidity",         "Humidity",     DATA_COND, humidity_ok, DATA_INT,    humidity,
+            "wind_dir_deg",     "",             DATA_INT,  wind_dir,
+            "rain",             "Rain?",        DATA_COND, rain_ok, DATA_INT,    rain_raw,
+            "uv",               "UV",           DATA_COND, uv_ok, DATA_FORMAT, "%.1f", DATA_DOUBLE,    uv,
+            "mic",              "Integrity",    DATA_STRING, "CHECKSUM",
+            NULL);
+    /* clang-format on */
+
+    decoder_output_data(decoder, data);
+    return 1;
+}
+
+/**
 Decoder for Bresser Weather Center 5-in-1.
 
 The compact 5-in-1 multifunction outdoor sensor transmits the data on 868.3 MHz.
@@ -54,9 +170,6 @@ Packet payload without preamble (203 bits):
 - B = Battery. 0=Ok, 8=Low.
 */
 
-#include "decoder.h"
-
-
 static int bresser_5in1_callback(r_device *decoder, bitbuffer_t *bitbuffer)
 {
     uint8_t const preamble_pattern[] = {0xaa, 0xaa, 0xaa, 0x2d, 0xd4};
@@ -65,6 +178,12 @@ static int bresser_5in1_callback(r_device *decoder, bitbuffer_t *bitbuffer)
     uint8_t msg[26];
     uint16_t sensor_id;
     unsigned len = 0;
+
+    if (bitbuffer->num_rows == 1
+            && bitbuffer->bits_per_row[0] > 160
+            && bitbuffer->bits_per_row[0] < 220) {
+        return bresser_6in1_callback(decoder, bitbuffer);
+    }
 
     if (bitbuffer->num_rows != 1
             || bitbuffer->bits_per_row[0] < 248
